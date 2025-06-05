dc52ea5861d89bb9ac2fa9af9dfe6898177a0e5f
Arnav Kapur <arnavkapur06332@gmail.com>
2025-06-05 23:30:33 +0530

    First.java file Modified

--- Fundamental Concepts/First.java
+++ Fundamental Concepts/First.java
@@ -1,5 +1,6 @@
+// The following code contains the first program in java
 public class First{
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("First Program in Java.");
     }
 }
