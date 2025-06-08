11905a1900d199a32f41f01770b83b778396e3fb
Arnav Kapur <arnavkapur06332@gmail.com>
2025-06-08 15:01:36 +0530

    Changes to the read me file

--- Fundamental Concepts/First.java
+++ Fundamental Concepts/First.java
@@ -1,6 +1,7 @@
 // The following code contains the first program in java
 public class First{
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("First Program in Java.");
     }
 }
